40f659737dc8d92a9ccf2939d027c7f018aace32
schinken <schinken@hackerspace-bamberg.de>
2013-02-01 20:25:07 +0100

    changed api url

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ font_list = {
     'DejaVu Sans Mono': '/usr/share/fonts/TTF/DejaVuSansMono.ttf'    
 }
 
-ledloard_host = 'ledloard.bckspc.de'
+ledloard_host = 'ledloard.core.bckspc.de'
 ledloard_port = 1337
